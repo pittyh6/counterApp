0872058e0b361ac5add742c0de2c48e042de403b
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-12-01 17:55:04 +1100

    add fields button and input and create function

--- counterApp-reactjs/src/App.jsx
+++ counterApp-reactjs/src/App.jsx
@@ -6,14 +6,34 @@ import Counter from "./components/Counter";
 function App() {
   const [count, setCount] = useState(0);
 
+  function reset() {
+    setCount(0);
+    console.log(count);
+  }
+  function add() {
+    let addValue = count + 1;
+    setCount(addValue);
+  }
+  function sub() {
+    let subValue = count - 1;
+    setCount(subValue);
+  }
+
   return (
     <main>
       <h1>Count</h1>
       <section className="counting">
-        <p>0</p>
-        <button className="plus">+</button>
-        <button className="sub">-</button>
-        <button className="reset">Reset</button>
+        <p>{count}</p>
+        <input type="number" placeholder="Insert a value for steps" />
+        <button className="plus" onClick={add}>
+          +
+        </button>
+        <button className="sub" onClick={sub}>
+          -
+        </button>
+        <button className="reset" onClick={reset}>
+          Reset
+        </button>
       </section>
     </main>
   );
