64f09ba705728f0bafef315566abf3bd1de39488
Priscila Ferreira Bezerra <pittyh6@gmail.com>
2024-12-11 11:26:50 +1100

    screen elements

--- counterApp-reactNative/app/screens/CounterScreen.js
+++ counterApp-reactNative/app/screens/CounterScreen.js
@@ -1,23 +1,70 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableHighlight, } from 'react-native';
 
 import color from '../config/colors'
 
 function CounterScreen(props) {
     return (
         <View style={styles.container}>
-            <Text>Hi</Text>
-        </View>
+            <Text style={{ fontWeight: 'bold', fontSize: 36 }}>Count</Text>
+            <View>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Insert Value"
+                    keyboardType="numeric"
+                />
+                <View style={styles.buttonArea}>
+                    <TouchableHighlight onPress={() => console.log("Pressed +")}>
+                        <View style={styles.button}>
+                            <Text>+</Text>
+                        </View>
+                    </TouchableHighlight>
+                    <TouchableHighlight onPress={() => console.log("Pressed -")}>
+                        <View style={styles.button}>
+                            <Text>-</Text>
+                        </View>
+                    </TouchableHighlight>
+                    <TouchableHighlight onPress={() => console.log("Pressed Reset")}>
+                        <View style={[styles.button, styles.buttonReset]}>
+                            <Text>Reset</Text>
+                        </View>
+                    </TouchableHighlight>
+                </View>
+            </View>
+        </View >
+
     );
 }
 
 const styles = StyleSheet.create({
+    button: {
+        width: 50,
+        height: 30,
+        backgroundColor: color.buttonBg,
+        borderRadius: 10,
+        color: color.white,
+        marginVertical: 10,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    buttonArea: {
+    },
+    buttonReset: {
+        width: 200,
+    },
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: color.bg,
         alignItems: 'center',
         justifyContent: 'center',
     },
+    input: {
+        width: 200,
+        height: 30,
+        backgroundColor: color.white,
+        padding: 5,
+    },
+
 });
 
 export default CounterScreen;
